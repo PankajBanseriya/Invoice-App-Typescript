ccf838ee4977a0ed532668aad686fe088febe12a
PankajBanseriya <pankajbanseriya@gmail.com>
2026-04-28 11:56:40 +0530

    Remove unused QueryClientProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
         <Toaster />
         <App />
-      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
